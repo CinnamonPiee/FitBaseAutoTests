ab2a8f4af7482d5e4dd860e5d20e0d966ebbc13e
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-02-09 15:06:58 +0400

    create tests locators in base and auth pages

--- pages/locators.py
+++ pages/locators.py
@@ -1,2 +1,16 @@
+from typing import Literal
+from selenium.webdriver.common.by import By
+
+
+class BasePageLocators:
+	USER_ICON = (By.CSS_SELECTOR, ".header-profile .user-profile")
+
+
+class AuthPageLocators:
+	LOGIN_FORM_USERNAME = (By.CSS_SELECTOR, "#loginform-username")
+	LOGIN_FORM_PASSWORD = (By.CSS_SELECTOR, "#loginform-password")
+	SUBMIT_BUTTON = (By.CSS_SELECTOR, ".btn")
+
+
 class MainPageLocators:
 	pass
