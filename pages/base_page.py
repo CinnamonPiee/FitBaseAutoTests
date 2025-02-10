b8a46563f2dd928d402dcf6c7993ab0c3f16921f
CinnamonPie <vonahkahs.s.ved@gmail.com>
2025-02-10 12:41:27 +0400

    add locators on base page in header and write check element on page in base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -11,6 +11,7 @@ from .locators import BasePageLocators
 
 from typing import Any
 import os
+import pytest
 
 
 load_dotenv()
@@ -59,6 +60,27 @@ class BasePage:
     
     def should_be_user_icon(self) -> None:
         assert self.is_element_present(*BasePageLocators.USER_ICON), "User icon is not visible on page!"
+
+    def should_be_change_club(self) -> None:
+        assert self.is_element_present(*BasePageLocators.CHANGE_CLUB_FORM), "Change club button is not visible on page!"
+
+    def should_be_header_idea(self) -> None:
+        assert self.is_element_present(*BasePageLocators.HEADER_IDEA), "Header idea is not visible on page!"
+
+    def should_be_header_knowleage_base(self) -> None:
+        assert self.is_element_present(*BasePageLocators.HEADER_KNOWLEAGE_BASE), "Header knowleage base is not visible on page!"
+
+    def should_be_banner_header_limit(self) -> None:
+        assert self.is_element_present(*BasePageLocators.BANNER_HEADER_LIMIT), "Banner header limit is not visible on page!"
+
+    def should_be_fast_client_search(self) -> None:
+        assert self.is_element_present(*BasePageLocators.FAST_CLIENT_SEARCH), "Fast client search is not visible on page!"
+
+    def should_be_fast_client_button(self) -> None:
+        assert self.is_element_present(*BasePageLocators.FAST_CLIENT_BUTTON), "Fast client button is not visible on page!"
+
+    def should_be_open_clients_reception(self) -> None:
+        assert self.is_element_present(*BasePageLocators.OPEN_CLIENTS_RECEPTION), "Open clients reception is not visible on page!"
 
     def click_on_user_icon(self) -> None:
         link = self.browser.find_element(*BasePageLocators.USER_ICON)
--- pages/locators.py
+++ pages/locators.py
@@ -2,16 +2,30 @@ from selenium.webdriver.common.by import By
 
 
 class BasePageLocators:
+	# Верхняя часть страницы
 	USER_ICON = (By.CSS_SELECTOR, ".header-profile .user-profile")
+	HEADER_KNOWLEAGE_BASE = (By.CSS_SELECTOR, ".header_knowleage_base .btn-primary")
+	HEADER_IDEA = (By.CSS_SELECTOR, ".header_idea .btn-primary")
+	BANNER_HEADER_LIMIT = (By.CSS_SELECTOR, ".banner_header-limit")
+	# TODO Добавить css селекторы для всплывающего списка клиентов
+	FAST_CLIENT_SEARCH = (By.CSS_SELECTOR, "#fast-client-card-input-main")
+	FAST_CLIENT_BUTTON = (By.CSS_SELECTOR, ".btn-success")
+	# TODO Добавить css селекторы для всплывающего табло клиентов в клубе
+	OPEN_CLIENTS_RECEPTION = (By.CSS_SELECTOR, "#open_clients_reception")
+	# TODO Добавить css селекторы для всплывающего списка
+	CHANGE_CLUB_FORM = (By.CSS_SELECTOR, "#change-club-form")
 
 
 class AuthPageLocators:
+	# Страница авторизации
 	LOGIN_FORM_USERNAME = (By.CSS_SELECTOR, "#loginform-username")
 	LOGIN_FORM_PASSWORD = (By.CSS_SELECTOR, "#loginform-password")
 	SUBMIT_BUTTON = (By.CSS_SELECTOR, ".btn")
 	CARROTQUEST_SUPPORT_PALM = (By.CSS_SELECTOR, "#carrot-messenger-collapsed-frame")
 	LOGIN_FORM_TOGGLE = (By.CSS_SELECTOR, ".login-form-toggle")
 	LOGIN_FORM_REMEMBERME = (By.CSS_SELECTOR, ".field-loginform-rememberme")
+
+	# Страница сброса пароля
 	PASSWORD_RESET_REQUEST_FORM = (By.CSS_SELECTOR, "#passwordresetrequestform-username")
 	PASSWORD_RESET_REQUEST_BUTTON = (By.CSS_SELECTOR, ".btn-lg")
 	LOGIN_FORM_TOGGLE_COMEBACK = (By.XPATH, "//section[@id='passwordResetForm']//div[@class='form_row']")
